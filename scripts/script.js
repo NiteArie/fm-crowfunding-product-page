390cfff249744ccdf9a267300e9676a6d5f5eb85
Nite <solitude.loner7@gmail.com>
2021-02-23 23:12:31 +0700

    [Fea] Modal popup via button click

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,11 @@
     const bookmarkProjectButton = document.querySelector(".js-bookmark");
     const bookmarkProjectImage = document.querySelector(".js-bookmark-img");
 
+    const modal = document.querySelector(".js-modal");
+    const modalBackground = document.querySelector(".js-modal-bg");
+
     var bookmarked = false;
+    var modalHiddenState = true;
 
     bookmarkProjectButton.addEventListener("click", (event) => {
 
@@ -16,6 +20,16 @@
         }
     })
 
+    backProjectButton.addEventListener("click", (event) => {
+        modalHiddenState = !modalHiddenState;
+
+        if ( !modalHiddenState ) {
+            showModal();
+        } else {
+            hideModal();
+        }
+    });
+
     function changeBookmarkButtonToSuccess() {
         bookmarkProjectButton.classList.add("welcome__action--bookmarked");
         bookmarkProjectImage.classList.add("welcome__bookmark--bookmarked");
@@ -24,5 +38,16 @@
     function changeBookMarkButtonToNormal() {
         bookmarkProjectButton.classList.remove("welcome__action--bookmarked");
         bookmarkProjectImage.classList.remove("welcome__bookmark--bookmarked");
+    }
+
+    function showModal() {
+        console.log("WTF");
+        modal.classList.remove("modal--hidden");
+        modalBackground.classList.remove("background--hidden");
+    }
+
+    function hideModal() {
+        modal.classList.add("modal--hidden");
+        modalBackground.classList.add("background--hidden");
     }
 })();
